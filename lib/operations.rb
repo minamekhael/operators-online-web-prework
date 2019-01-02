d8402d0d17f9cc6bf42cbabcf41ffaf374c62d05
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-02 01:13:14 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed = speed > 60 || speed < 40
+    puts
+    true
+  else speed
+    puts
+    false
 end
-
-
+end
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
-
-
