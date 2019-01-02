3b92ec127bec7fbec8df124665833cbd72d3c374
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-02 00:24:41 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+  if speed == speed >= 60
+    print 
+      true
+  elsif  speed <= 40 
+    print
+      true
+  else speed 
+    print 
+      false
 end
-
-
+end
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
-
-
